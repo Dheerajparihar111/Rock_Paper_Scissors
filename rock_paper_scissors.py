7116268643a1b2e0a23d6c2fdef58d7deb2f78c1
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-05-03 15:28:18 +0530

    added Emoji to output

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,23 +8,23 @@ class RPS(Enum):
     scissors = 3
 
 print("welcome to rock paper and scissors")
-choices = input("1. rock\n2. paper\n3. scissors\n ")
+choices = input("1. rock🪨\n2. paper📄\n3. scissors✂️\n ")
 choice = int(choices)
-computer_choice = random.choice(["rock", "paper", "scissors"])
-print("you chose "+str(RPS(choice)).replace("RPS.rock", "rock ").replace("RPS.paper", "paper ").replace("RPS.scissors", "scissors"))
+computer_choice = random.choice(["rock 🪨", "paper 📄", "scissors ✂️"])
+print("you chose "+str(RPS(choice)).replace("RPS.rock", "rock 🪨").replace("RPS.paper ", "paper 📄").replace("RPS.scissors", "scissors ✂️"))
 print("python chose",computer_choice)
 
 if choice == 1 and computer_choice == "scissors":
-    print("you win")
+    print("🎊 you win")
 elif choice == 2 and computer_choice == "rock":
-    print("you win")
+    print("🎊 you win")
 elif  choice == 3 and computer_choice == "paper":
-    print("you win")
+    print("🎊 you win")
 elif choice == 1 and computer_choice == "rock":
-    print("it`s a draw")
+    print(" 🤝 it`s a draw")
 elif choice == 2 and computer_choice == "paper":
-    print("it`s a draw")
+    print("🤝 it`s a draw")
 elif choice == 3 and computer_choice == "scissors":
-    print("it`s a draw")
+    print("🤝 it`s a draw")
 else:
-    print("Python Win")
+    print("🐍Python Win")
