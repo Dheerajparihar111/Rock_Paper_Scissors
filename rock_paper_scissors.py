6713c91fb1a67f9c8ab71b44fe30cc210f996253
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-05-03 15:46:59 +0530

    changed the position of emojis

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,8 @@
 import random
 from enum import Enum
 
+import sys
+
 
 class RPS(Enum):
     rock = 1
@@ -10,21 +12,22 @@ class RPS(Enum):
 print("welcome to rock paper and scissors")
 choices = input("1. rock🪨\n2. paper📄\n3. scissors✂️\n ")
 choice = int(choices)
+
 computer_choice = random.choice(["rock 🪨", "paper 📄", "scissors ✂️"])
-print("you chose "+str(RPS(choice)).replace("RPS.rock", "rock 🪨").replace("RPS.paper ", "paper 📄").replace("RPS.scissors", "scissors ✂️"))
+print("you chose "+str(RPS(choice)).replace("RPS.rock", "rock 🪨").replace("RPS.paper", "paper 📄").replace("RPS.scissors", "scissors ✂️"))
 print("python chose",computer_choice)
 
-if choice == 1 and computer_choice == "scissors":
-    print("🎊 you win")
-elif choice == 2 and computer_choice == "rock":
-    print("🎊 you win")
-elif  choice == 3 and computer_choice == "paper":
-    print("🎊 you win")
-elif choice == 1 and computer_choice == "rock":
-    print(" 🤝 it`s a draw")
-elif choice == 2 and computer_choice == "paper":
-    print("🤝 it`s a draw")
-elif choice == 3 and computer_choice == "scissors":
-    print("🤝 it`s a draw")
+if choice == 1 and computer_choice == "scissors ✂️":
+    print("you win 🎊")
+elif choice == 2 and computer_choice == "rock 🪨":
+    print("you win 🎊")
+elif  choice == 3 and computer_choice == "paper 📄":
+    print("you win 🎊")
+elif choice == 1 and computer_choice == "rock 🪨":
+    print("it`s a draw 🤝")
+elif choice == 2 and computer_choice == "paper 📄":
+    print("it`s a draw 🤝")
+elif choice == 3 and computer_choice == "scissors ✂️":
+    print("it`s a draw 🤝")
 else:
-    print("🐍Python Win")
+    print("Python Win 🐍")
