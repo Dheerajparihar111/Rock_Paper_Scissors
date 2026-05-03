5118efe289ff99950af82c149bf202c11f064a4e
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-05-03 11:46:17 +0530

    used condistional statement to decide the winner

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,3 +5,12 @@ choice = int(choices)
 computer_choice = random.choice(["rock", "paper", "scissors"])
 print("you chose",choice)
 print("python chose",computer_choice)
+
+if choice == 1 and computer_choice == "scissors":
+    print("you win")
+elif choice == 2 and computer_choice == "rock":
+    print("you win")
+elif choice == 3 and computer_choice == "paper":
+    print("you win")
+else:
+    print("Python Win")
