97e70ce23dba51cd9057fbe85aa7b98c6758c7f4
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-05-03 11:37:26 +0530

    printed user choice and python choice using random module

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,7 @@
+import random
 print("welcome to rock paper and scissors")
 choices = input("1. rock\n2. paper\n3. scissors\n ")
-print(choices)
+choice = int(choices)
+computer_choice = random.choice(["rock", "paper", "scissors"])
+print("you chose",choice)
+print("python chose",computer_choice)
